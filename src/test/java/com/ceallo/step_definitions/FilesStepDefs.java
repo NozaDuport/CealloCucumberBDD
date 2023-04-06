4470fefcb4fc1297bd4c87e03c588857ffb16e5c
Alev Sametoglu <alevsametoglu.fr@gmail.com>
2023-04-06 22:07:12 +0200

    create   file step definitions

---
+++ src/test/java/com/ceallo/step_definitions/FilesStepDefs.java
@@ -0,0 +1,64 @@
+package com.ceallo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+
+public class FilesStepDefs {
+    @Given("user on the file page")
+    public void user_on_the_file_page() {
+
+    }
+    @Given("user click three dot menu")
+    public void user_click_three_dot_menu() {
+
+    }
+    @When("user click add to favorite button")
+    public void user_click_add_to_favorite_button() {
+
+    }
+    @Then("user should see the file on favorite page")
+    public void user_should_see_the_file_on_favorite_page() {
+
+    }
+
+
+    @When("user click rename button")
+    public void user_click_rename_button() {
+
+    }
+    @Then("user should able to write on file name")
+    public void user_should_able_to_write_on_file_name() {
+
+    }
+
+
+    @When("user click detail button")
+    public void user_click_detail_button() {
+
+    }
+    @When("user click comment button")
+    public void user_click_comment_button() {
+
+    }
+    @Then("user should able to write comment input")
+    public void user_should_able_to_write_comment_input() {
+
+    }
+
+
+
+    @When("user click delete button")
+    public void user_click_delete_button() {
+
+    }
+    @Then("user should not see file on all file list")
+    public void user_should_not_see_file_on_all_file_list() {
+
+    }
+
+
+
+
+}
