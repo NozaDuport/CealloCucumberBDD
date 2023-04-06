3357bfaf329b065f3beb11dba18130cdef8eabc8
Alev Sametoglu <alevsametoglu.fr@gmail.com>
2023-04-06 22:41:35 +0200

    change dryRun to false

--- src/test/java/com/ceallo/runners/CukesRunner.java
+++ src/test/java/com/ceallo/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/ceallo/step_definitions",
         tags = "",
-        dryRun = true
+        dryRun = false
 )
 
 public class CukesRunner {
