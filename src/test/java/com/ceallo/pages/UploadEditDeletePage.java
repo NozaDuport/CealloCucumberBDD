c1962d2cf4435776dbb5e27fbe8024c882a8a366
Gorkem Agac <gorkem_agac@hotmail.com>
2023-04-07 22:20:47 +0100

    My first scenario

---
+++ src/test/java/com/ceallo/pages/UploadEditDeletePage.java
@@ -0,0 +1,24 @@
+package com.ceallo.pages;
+
+import com.ceallo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class UploadEditDeletePage {
+
+    public UploadEditDeletePage()
+    {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (xpath = "//div[@id='uploadprogresswrapper']/preceding-sibling::a")
+    public WebElement plusButton;
+
+    @FindBy (xpath = "//a[@data-templatename = 'New folder']")
+    public WebElement newFolderButton;
+
+    @FindBy(xpath = "//span[text()='test123']")
+    public WebElement createdTest123File;
+
+}
---
+++ src/test/java/com/ceallo/step_definitions/UploadEditDeleteStepDefs.java
@@ -0,0 +1,39 @@
+package com.ceallo.step_definitions;
+
+import com.ceallo.pages.UploadEditDeletePage;
+import com.ceallo.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class UploadEditDeleteStepDefs {
+
+    UploadEditDeletePage uploadEditDeletePage = new UploadEditDeletePage();
+
+    @When("user clicks plus button")
+    public void user_clicks_plus_button() {
+        // Write code here that turns the phrase above into concrete actions
+        uploadEditDeletePage.plusButton.click();
+
+    }
+    @When("clicks New folder")
+    public void clicks_new_folder() {
+        // Write code here that turns the phrase above into concrete actions
+        uploadEditDeletePage.newFolderButton.click();
+
+    }
+    @When("types {string} of the folder and hits enter")
+    public void types_of_the_folder_and_hits_enter(String test123Keyword) {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement activeElement = Driver.getDriver().switchTo().activeElement();
+        activeElement.sendKeys(test123Keyword + Keys.ENTER);
+    }
+    @Then("file gets created")
+    public void file_gets_created() {
+        // Write code here that turns the phrase above into concrete actions
+        Assert.assertTrue(uploadEditDeletePage.createdTest123File.isDisplayed());
+
+    }
+}
