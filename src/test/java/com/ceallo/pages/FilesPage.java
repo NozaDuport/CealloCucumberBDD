45b1d8c75b5671f1cfc7c702a9a91e1ef009e7d2
Alev Sametoglu <alevsametoglu.fr@gmail.com>
2023-04-10 01:28:15 +0200

    add web elements

--- src/test/java/com/ceallo/pages/FilesPage.java
+++ src/test/java/com/ceallo/pages/FilesPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class FilesPage {
     public FilesPage() {
 
@@ -20,7 +22,41 @@ public class FilesPage {
     @FindBy(xpath = "//li[@class=\" action-details-container\"]")
     public WebElement detailButton;
 
-    @FindBy(xpath = "//li[@class=\" action-delete-container\"]")
-    public WebElement deleteButton;
+    @FindBy(xpath = "//li[@class=\" action-rename-container\"]")
+    public WebElement renameButton;
+
+
+//    //tbody[@id="fileList"]/tr[@data-type="dir"]/td/a/span/span[.="Talk"]
+
+    @FindBy(xpath = "(//div[@id=\"app-navigation\"]/ul/li)[3]")
+    public WebElement favorites;
+
+    @FindBy(xpath = " //a[@data-id=\"commentsTabView\"]")
+    public  WebElement commentButton;
+
+    @FindBy(xpath ="//div[@data-placeholder=\"New comment …\"]" )
+    public WebElement commentInput;
+
+
+    @FindBy(xpath = "//li[@class=\"comment\"]")
+    public List<WebElement> comments;
+
+
+    @FindBy(xpath = "(//ul[@class=\"comments\"]/li/div[@class=\"message\"])[1]")
+    public WebElement addedComment;
+
+
+
+    @FindBy(xpath = "//tbody[@id=\"fileList\"]/tr[@data-type=\"dir\"]/td/a/span/span")
+
+    public List<WebElement> favoriFilesList;
+
+    @FindBy(xpath = "//a[@class =\"menuitem action delete permanent\"]/span[.=\"Delete comment\"]")
+    public WebElement deleteCommentButton;
+
+    @FindBy(xpath = "//a[@class =\"action more icon icon-more has-tooltip\"]")
+    public WebElement threeDotDetailPage;
+
+
 
 }
