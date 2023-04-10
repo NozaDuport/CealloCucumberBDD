a484d07c2d15e15ada8d61b1ad409b0d94e7831c
Adem OZTURK <admoztrk86@gmail.com>
2023-04-10 15:34:14 +0200

    New implementations are done

--- src/test/java/com/ceallo/step_definitions/ContactsStepDefs.java
+++ src/test/java/com/ceallo/step_definitions/ContactsStepDefs.java
@@ -16,46 +16,63 @@ public class ContactsStepDefs {
 
     @Given("the user is on contacts module page")
     public void the_user_is_on_contacts_module_page() {
+
         homePage.contactsModule.click();
+        BrowserUtils.sleep(4);
     }
 
     @When("the user clicks on new contact button")
     public void the_User_clicks_On_new_contact_button() {
 
         contacts.newContactButton.click();
-        BrowserUtils.sleep(4);
+
     }
+
+    // First Scenario
     @And("the user clicks on add new property option")
     public void the_User_Clicks_On_Add_New_Property_Option() {
         contacts.addNewProperty.click();
-        BrowserUtils.sleep(4);
+
     }
 
     @And("the user clicks on detailed name option")
     public void the_User_Clicks_On_detailed_Name_Option() {
         contacts.detailedNameOption.click();
-        BrowserUtils.sleep(4);
+
+
     }
     @When("the user enters contact name {string}")
     public void the_user_enters_contact_name(String contactName) {
         contacts.name.sendKeys(contactName);
-        BrowserUtils.sleep(4);
+        BrowserUtils.sleep(3);
+
     }
     @When("the user enters phone number {string}")
     public void the_user_enters_phone_number(String phoneNumber) {
 
         contacts.phoneBox.sendKeys(phoneNumber);
-        BrowserUtils.sleep(4);
+        BrowserUtils.sleep(3);
     }
 
     @When("the user enters email {string}")
     public void the_user_enters_email(String email) {
 
         contacts.emailBox.sendKeys(email);
-        BrowserUtils.sleep(4);
+        BrowserUtils.sleep(3);
+
     }
 
+    @And("the user enters address {string}")
+    public void the_User_Enters_Address(String address) {
+        contacts.addressBox.sendKeys(address);
+        BrowserUtils.sleep(3);
+    }
 
+    @And("the user enters country {string}")
+    public void the_User_Enters_Country(String country) {
+        contacts.countryBox.sendKeys(country);
+        BrowserUtils.sleep(3);
+    }
 
     @Then("the user should display new created contact")
     public void the_user_should_display_new_created_contact() {
@@ -63,6 +80,14 @@ public class ContactsStepDefs {
 
     }
 
+    // Second Scenario
+    @When("user clicks all contacts button")
+    public void userClicksAllContactsButton() {
+    }
+
+    @Then("the user should display all the contacts near the contacts tab")
+    public void theUserShouldDisplayAllTheContactsNearTheContactsTab() {
+    }
 
 
 }
