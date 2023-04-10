7ef2cf5f2272ea7940ac3629b32a4c37514d0d0f
Adem OZTURK <admoztrk86@gmail.com>
2023-04-10 13:19:28 +0200

    Contact defs updated

--- src/test/java/com/ceallo/step_definitions/ContactsStepDefs.java
+++ src/test/java/com/ceallo/step_definitions/ContactsStepDefs.java
@@ -2,6 +2,8 @@ package com.ceallo.step_definitions;
 
 import com.ceallo.pages.ContactsPage;
 import com.ceallo.pages.HomePage;
+import com.ceallo.utilities.BrowserUtils;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -19,50 +21,48 @@ public class ContactsStepDefs {
 
     @When("the user clicks on new contact button")
     public void the_User_clicks_On_new_contact_button() {
+
         contacts.newContactButton.click();
+        BrowserUtils.sleep(4);
+    }
+    @And("the user clicks on add new property option")
+    public void the_User_Clicks_On_Add_New_Property_Option() {
+        contacts.addNewProperty.click();
+        BrowserUtils.sleep(4);
     }
 
+    @And("the user clicks on detailed name option")
+    public void the_User_Clicks_On_detailed_Name_Option() {
+        contacts.detailedNameOption.click();
+        BrowserUtils.sleep(4);
+    }
+    @When("the user enters contact name {string}")
+    public void the_user_enters_contact_name(String contactName) {
+        contacts.name.sendKeys(contactName);
+        BrowserUtils.sleep(4);
+    }
     @When("the user enters phone number {string}")
     public void the_user_enters_phone_number(String phoneNumber) {
+
         contacts.phoneBox.sendKeys(phoneNumber);
+        BrowserUtils.sleep(4);
     }
 
     @When("the user enters email {string}")
     public void the_user_enters_email(String email) {
+
         contacts.emailBox.sendKeys(email);
+        BrowserUtils.sleep(4);
     }
 
-    @When("the user enters address {string}")
-    public void the_user_enters_address(String address) {
-        contacts.addressBox.sendKeys(address);
-    }
 
-    @When("the user enters postal code {string}")
-    public void the_user_enters_postal_code(String postCode) {
-
-        contacts.postcodeBox.sendKeys(postCode);
-    }
-
-    @When("the user enters city {string}")
-    public void the_user_enters_city(String city) {
-        contacts.cityBox.sendKeys(city);
-    }
-
-    @When("the user enters state {string}")
-    public void the_user_enters_state(String state) {
-        contacts.stateBox.sendKeys(state);
-    }
-
-    @When("the user enters country {string}")
-    public void the_user_enters_country(String country) {
-        contacts.countryBox.sendKeys(country);
-    }
 
     @Then("the user should display new created contact")
     public void the_user_should_display_new_created_contact() {
-        Assert.assertTrue(contacts.contactName.isDisplayed());
+        Assert.assertTrue(contacts.contactCheck.isDisplayed());
 
     }
+
 
 
 }
