1f4cf062cae2949e4ab185804dc342580b52008d
Adem OZTURK <admoztrk86@gmail.com>
2023-04-10 21:53:00 +0200

    new changes are done

--- src/test/java/com/ceallo/step_definitions/ContactsStepDefs.java
+++ src/test/java/com/ceallo/step_definitions/ContactsStepDefs.java
@@ -8,6 +8,10 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ContactsStepDefs {
 
@@ -41,12 +45,14 @@ public class ContactsStepDefs {
 
 
     }
+
     @When("the user enters contact name {string}")
     public void the_user_enters_contact_name(String contactName) {
         contacts.name.sendKeys(contactName);
         BrowserUtils.sleep(3);
 
     }
+
     @When("the user enters phone number {string}")
     public void the_user_enters_phone_number(String phoneNumber) {
 
@@ -82,12 +88,78 @@ public class ContactsStepDefs {
 
     // Second Scenario
     @When("user clicks all contacts button")
-    public void userClicksAllContactsButton() {
+    public void user_Clicks_All_Contacts_Button() {
+
+        contacts.allContactsButton.click();
     }
 
-    @Then("the user should display all the contacts near the contacts tab")
-    public void theUserShouldDisplayAllTheContactsNearTheContactsTab() {
+    @Then("the user should display all the contacts inside the middle column and total number of contacts near the contacts tab")
+    public void the_User_Should_Display_All_The_Contacts_Inside_The_Middle_Column_And_Total_Number_Of_contacts_Near_the_contacts_Tab() {
+
+
+       List<String> allContactsNames= Arrays.asList("Adem","Arten","Valentine","calbares");
+
+        List<WebElement> actualListOfContacts = contacts.allContacts;
+        for (WebElement actualListOfContact : actualListOfContacts) {
+            if (actualListOfContact.equals(allContactsNames)) {
+                Assert.assertEquals(actualListOfContact.getText(), allContactsNames);
+            }
+
+        }
+
+
+        String expectedCount = contacts.contactsCount.getText();
+
+        int actualContactsCount = actualListOfContacts.size();
+        int expectedContactsCount = Integer.parseInt(expectedCount);
+
+        System.out.println(actualListOfContacts.size());
+        Assert.assertEquals(expectedContactsCount, actualContactsCount);
+
+
     }
 
+
+    // Third Scenario
+    @When("the user clicks on one of the contacts in the contact list")
+    public void the_user_clicks_on_one_of_the_contacts_in_the_contact_list() {
+
+    }
+
+    @When("the user clicks on change picture button placed on the profile picture")
+    public void the_user_clicks_on_change_picture_button_placed_on_the_profile_picture() {
+
+    }
+
+    @When("the user clicks chose from files file")
+    public void the_user_clicks_chose_from_files_file() {
+
+    }
+
+    @When("the user clicks on the picture that he wants to change")
+    public void the_user_clicks_on_the_picture_that_he_wants_to_change() {
+
+    }
+
+    @Then("user should be displayed new profile picture in the contacts list")
+    public void user_should_be_displayed_new_profile_picture_in_the_contacts_list() {
+
+    }
+
+    // fourth scenario
+    @When("the user clicks on three dots button left on the page")
+    public void the_user_clicks_on_three_dots_button_left_on_the_page() {
+
+    }
+
+    @When("the user clicks on the delete button")
+    public void the_user_clicks_on_the_delete_button() {
+
+    }
+
+    @Then("the user should delete the selected contact in the contacts list")
+    public void the_user_should_delete_the_selected_contact_in_the_contacts_list() {
+
+    }
 
 }
