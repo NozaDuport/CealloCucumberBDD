9dcfcbc2a90553a85eda633606dace62afe9090e
Adem OZTURK <admoztrk86@gmail.com>
2023-04-13 03:05:54 +0200

    main tag added

--- src/test/java/com/ceallo/runners/CukesRunner.java
+++ src/test/java/com/ceallo/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/ceallo/step_definitions",
         dryRun = false,
-        tags = "@adem"
+        tags = "@CEA10-391"
 )
 
 public class CukesRunner {
