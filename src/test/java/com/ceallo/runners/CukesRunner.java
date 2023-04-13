345a3e205f0feeac60eec8fee60fbd174c14fea2
NozaDuport <gulnozik@yahoo.com>
2023-04-13 21:50:14 +0200

    fourth scenario passed

--- src/test/java/com/ceallo/runners/CukesRunner.java
+++ src/test/java/com/ceallo/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/ceallo/step_definitions",
         dryRun = false,
-        tags = "@CEA10-343"
+        tags = ""
 )
 
 public class CukesRunner {
